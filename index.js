5910250c03984873f0b0d7647a815ec4fb9d31a0
Kellen-Linse <kblinse06@gmail.com>
2022-01-17 13:49:42 -0800

    Test for working server

--- index.js
+++ index.js
@@ -32,14 +32,16 @@ const Users = sequelize.define('User', {
 // Signup Route -- create a new user
 // Two ways to test this route with httpie
 // echo '{"username":"john","password":"foo"}' | http post :3000/signup
-// http post :3000/signup usernmae=john password=foo
+// http post :3000/signup username=john password=foo
 app.post('/signup', async (req, res) => {
 
   try {
     req.body.password = await bcrypt.hash(req.body.password, 10);
     const record = await Users.create(req.body);
     res.status(200).json(record);
-  } catch (e) { res.status(403).send("Error Creating User"); }
+  } catch (e) { 
+    res.status(403).send("Error Creating User"); 
+  }
 });
 
 
